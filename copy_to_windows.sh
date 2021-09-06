4faa0c0560b3ca9830950e48afae90e517554626
Aakash Sethi <aakash.k.sethi@gmail.com>
2021-09-05 17:36:29 -0700

    Add helper script to copy to windows

---
+++ copy_to_windows.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cp build/src/blinky /mnt/c/Users/aakas/Documents/Code/NRF51/
+cp build/src/blinky.hex /mnt/c/Users/aakas/Documents/Code/NRF51/
+cp build/src/blinky.bin /mnt/c/Users/aakas/Documents/Code/NRF51/
+date > /mnt/c/Users/aakas/Documents/Code/NRF51/build_timestamp.txt
